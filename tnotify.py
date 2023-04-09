a14499113472bc4e7fc861e3ef0a43813fcd0334
Sergey Parshin <quarck@gmail.com>
2023-04-09 02:54:39 +0100

    Templates for everything - updated

---
+++ tnotify.py
@@ -0,0 +1,19 @@
+import sys
+import urllib.request
+import auth
+
+BASE_URI = 'https://api.telegram.org/' + auth.tg_bot_api_token + '/sendMessage?%s'
+
+
+def notify(message: str):
+    if auth.tg_notify_user_id == 0 or auth.tg_bot_api_token == ""
+        return
+
+    params = urllib.parse.urlencode({'chat_id': auth.tg_notify_user_id, 'text': message})
+
+    with urllib.request.urlopen(BASE_URI % params) as f:
+        _ = f.read().decode('utf-8')
+
+
+if __name__ == "__main__":
+    notify("test message!")
